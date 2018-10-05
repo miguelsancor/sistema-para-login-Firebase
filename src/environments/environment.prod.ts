868e2e8f80985e75e5afaef39fd2c0fbd138b1ce
masanchezco <migsan@unisoft.com.co>
2018-10-04 23:04:00 -0500

    Firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig:{
+    apiKey: "AIzaSyDDLCJPV7oFDCxwyosjqTEpZrgxdEXeEfE",
+    authDomain: "loginapp-6c8f3.firebaseapp.com",
+    databaseURL: "https://loginapp-6c8f3.firebaseio.com",
+    projectId: "loginapp-6c8f3",
+    storageBucket: "loginapp-6c8f3.appspot.com",
+    messagingSenderId: "636951903047"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig:{
+    apiKey: "AIzaSyDDLCJPV7oFDCxwyosjqTEpZrgxdEXeEfE",
+    authDomain: "loginapp-6c8f3.firebaseapp.com",
+    databaseURL: "https://loginapp-6c8f3.firebaseio.com",
+    projectId: "loginapp-6c8f3",
+    storageBucket: "loginapp-6c8f3.appspot.com",
+    messagingSenderId: "636951903047"
+  }
 };
 
 /*
